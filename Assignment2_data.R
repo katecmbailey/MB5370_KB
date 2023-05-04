83dbca97629e763ca1fad395c9d2479522147cc9
katecmbailey <katherinecmbailey@outlook.com>
2023-05-04 13:45:58 +1000

    Saving assignment 2 folder

---
+++ Assignment2_data.R
@@ -0,0 +1,10 @@
+### ----------------- ###
+
+## Assignment - End-to-end data analysis in R ##
+
+## Import the data
+
+library(readr)
+QLD_sharkdata <- read_csv("E:/QLD_sharkdata.csv")
+View(QLD_sharkdata)
+
